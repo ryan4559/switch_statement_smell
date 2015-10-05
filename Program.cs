87cb96276ef2c63f4ca539c2af1a11ab687ad7b8
ryan4559 <ryan4559@gmail.com>
2015-10-05 11:46:36 +0800

    排版

--- Program.cs
+++ Program.cs
@@ -4,58 +4,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1 {
-    class Animal {
-        public Animal(string name) {
+namespace ConsoleApplication1
+{
+    class Animal
+    {
+        public Animal(string name)
+        {
             Name = name;
         }
         virtual public string AnimalType { get; private set; }
-        virtual public string Talk{ get; private set; }
+        virtual public string Talk { get; private set; }
         public string Name { get; private set; }
 
     }
 
-    class Dog : Animal {
-        public Dog(string name):base(name) {
+    class Dog : Animal
+    {
+        public Dog(string name)
+            : base(name)
+        {
 
         }
-        public override string AnimalType {
-            get {
+        public override string AnimalType
+        {
+            get
+            {
                 return "dog";
             }
         }
-        public override string Talk {
-            get {
+        public override string Talk
+        {
+            get
+            {
                 return "汪汪";
             }
         }
     }
 
-    class Cat : Animal {
-        public Cat(string name):base(name) {
+    class Cat : Animal
+    {
+        public Cat(string name)
+            : base(name)
+        {
 
         }
-        public override string AnimalType {
-            get {
+        public override string AnimalType
+        {
+            get
+            {
                 return "cat";
             }
         }
-        public override string Talk {
-            get {
+        public override string Talk
+        {
+            get
+            {
                 return "喵喵";
             }
         }
     }
 
-    class Program {
-        static void Main(string[] args) {
+    class Program
+    {
+        static void Main(string[] args)
+        {
             List<Animal> animals = new List<Animal>() {
                 new Dog("dog1"),
                 new Dog("dog2"),
                 new Cat("cat1")
             };
 
-            foreach (var animal in animals) {
+            foreach (var animal in animals)
+            {
                 Console.WriteLine(animal.Name + animal.Talk);
             }
         }
